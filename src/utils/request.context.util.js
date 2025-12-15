616bae2388340f5a1bb0a66f81b25d42c3edc226
gitesh152 <gitesh152@gmail.com>
2025-12-16 01:36:32 +0530

    fix request context

--- src/utils/request.context.util.js
+++ src/utils/request.context.util.js
@@ -2,7 +2,7 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 const asyncLocalStorage = new AsyncLocalStorage();
 
 const setContext = (data) => {
-  asyncLocalStorage.enterWith({ data });
+  asyncLocalStorage.enterWith(data);
 };
 
 const getContext = () => asyncLocalStorage.getStore() || {};
